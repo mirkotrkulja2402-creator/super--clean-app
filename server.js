2e1eda337bc60ff5aa0587ea1398f13601898aec
mirkotrkulja2402-creator <mirkotrkulja.2402@gmail.com>
2026-09-07 12:06:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2845,8 +2845,7 @@ app.get("/api/health", async (request, response, next) => {
     if (request.path.startsWith("/api/")) {
         return next();
     }
-app.get("/{*splat}", async (request, response, next) => {
-    try {
+app.get("/{*splat}", async (request, response, next) => { 
         const indexPath = path.join(__dirname, "public", "index.html");
         return response.send(await fs.readFile(indexPath, "utf8"));
     } catch (error) {
